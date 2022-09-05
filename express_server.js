f6184eb295e135e4ca4432f3da6492496e12dee7
originallykevin <ly1@ualberta.ca>
2022-09-05 20:30:59 +0000

    add new hello path to file

--- express_server.js
+++ express_server.js
@@ -15,6 +15,10 @@ app.get("/", (req, res) => {
   res.send("Hello Nori!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
