db2ee4877527f5f1f370c709c14e64d17461e13f
originallykevin <ly1@ualberta.ca>
2022-09-05 21:32:06 +0000

    update with templateVars

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
