c8d3f8f0507eccc6bc51890c8ea5e29739f77ffc
originallykevin <ly1@ualberta.ca>
2022-09-05 21:13:10 +0000

    add template file

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
