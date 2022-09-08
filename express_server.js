addee706148803cac9fe354b6b45544b195725f5
originallykevin <ly1@ualberta.ca>
2022-09-08 22:41:23 +0000

    install and add cookie-session. replace cookie-parser with seesion thru file

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
 
 const app = express();
@@ -8,9 +9,18 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+//////// MIDDLEWARE /////////
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(cookieSession({
+  name: 'cookieMonster',
+  keys: ['anything', 'something'],
+
+  // Cookie Options
+  // maxAge: 24 * 60 * 60 * 1000 // 24 hours
+
+}));
 
 // Old database
 // const urlDatabase = {
@@ -88,7 +98,7 @@ const generateRandomString = () => {
 // submit handle for new url items
 app.post("/urls", (req, res) => {
   // prevent users not logged in from POST /urls for security measures
-  const user_id = req.cookies.user_id;
+  const user_id = req.session.user_id;
   if (!user_id) {
     res.send('Please login to shorten URL');
   }
@@ -109,13 +119,14 @@ app.post('/login', (req, res) => {
   if (!user_id) {
     return res.status(403).send('No user with that username found');
   }
-  
+
   // compare user enter password vs. hashed password and return/false
   const result = bcrypt.compareSync(userPassword, users[user_id].password);
   // check if user email and user password match
   // result is a boolean, if true, client is logged in
   if (users[user_id].email === userEmail && result) {
-    res.cookie("user_id", user_id); // cookie with user_id
+    // res.cookie("user_id", user_id); // cookie with user_id
+    req.session.user_id = user_id;
     return res.redirect('/urls');
   } else {
     return res.status(403).send('username or password does not match');
@@ -123,7 +134,8 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie("user_id"); // once logout, cookies will be cleared
+  // res.clearCookie("user_id"); // once logout, cookies will be cleared
+  req.session = null;
   res.redirect('/urls');
 });
 
@@ -134,7 +146,7 @@ app.post('/urls/:id/delete', (req, res) => {
 });
 
 app.post('/urls/:id', (req, res) => {
-  const user_id = req.cookies['user_id'];
+  const user_id = req.session['user_id'];
   const id = req.params.id;
   // urlDatabase[id] = req.body.newURLname;
   urlDatabase[id] = {
@@ -162,27 +174,28 @@ app.post('/register', (req, res) => {
 
   // Move database under if statement above because we want to check email against existing database prior to adding it. 
   users[user_id] = {
-    id: user_id,
+    // id: user_id,
     email,
     password: hash // update password to hashed password
   };
-  console.log("users[user_id]", users[user_id])
-  res.cookie("user_id", user_id);
+  console.log("users[user_id]", users[user_id]);
+  // res.cookie("user_id", user_id);
+  req.session.user_id = user_id;
   return res.redirect('/urls');
 });
 
 // Cookie-parser
 app.get('/', function(req, res) {
   // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies);
+  console.log('Cookies: ', req.session);
   // Cookies that have been signed
   console.log('Signed Cookies: ', req.signedCookies);
 });
 
 app.get('/login', (req, res) => {
-  const user_id = req.cookies['user_id']
+  const user_id = req.session['user_id'];
   // if user is logged in, /login will redirect to /urls
-  if (req.cookies.user_id) {
+  if (req.session.user_id) {
     return res.redirect('/urls');
   }
   const templateVars = {
@@ -192,7 +205,7 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-  const user_id = req.cookies['user_id'];
+  const user_id = req.session['user_id'];
   // if user is logged in, /register will redirect to /urls
   if (user_id) {
     return res.redirect('/urls');
@@ -205,7 +218,7 @@ app.get('/register', (req, res) => {
 
 // BROWSE - GET /urls 
 app.get("/urls", (req, res) => {
-  const user_id = req.cookies['user_id'];
+  const user_id = req.session['user_id'];
   const templateVars = {
     urls: urlsForUser(urlDatabase, user_id),
     user: users[user_id], // this has value of generated id
@@ -230,7 +243,7 @@ app.get('/u/:id', (req, res) => {
 
 // GET req to add new url
 app.get("/urls/new", (req, res) => {
-  const user_id = req.cookies['user_id'];
+  const user_id = req.session['user_id'];
   // if user is not logged in, redirect to /login
   if (!user_id) {
     res.redirect('/login');
