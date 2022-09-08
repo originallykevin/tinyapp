044fac2365ec911d0443616c58f50bf06be7ef5a
originallykevin <ly1@ualberta.ca>
2022-09-08 21:33:54 +0000

    add password hash for login/registration page

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+
+const app = express();
+const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
@@ -57,7 +59,7 @@ const users = {
 // check if email is in users{ } for 
 const getUserByEmail = function(users, email) {
   for (let user in users) {
-    console.log(users[user].email);
+    // console.log(users[user].email);
     if (users[user].email === email) {
       return user;
     }
@@ -85,7 +87,6 @@ const generateRandomString = () => {
 
 // submit handle for new url items
 app.post("/urls", (req, res) => {
-
   // prevent users not logged in from POST /urls for security measures
   const user_id = req.cookies.user_id;
   if (!user_id) {
@@ -106,14 +107,18 @@ app.post('/login', (req, res) => {
   const user_id = getUserByEmail(users, userEmail);
   // check if user email found
   if (!user_id) {
-    return res.status(403).render('403');
+    return res.status(403).send('No user with that username found');
   }
+  
+  // compare user enter password vs. hashed password and return/false
+  const result = bcrypt.compareSync(userPassword, users[user_id].password);
   // check if user email and user password match
-  if (users[user_id].email === userEmail && users[user_id].password === userPassword) {
+  // result is a boolean, if true, client is logged in
+  if (users[user_id].email === userEmail && result) {
     res.cookie("user_id", user_id); // cookie with user_id
-    res.redirect('/urls');
+    return res.redirect('/urls');
   } else {
-    return res.status(403).render('403');
+    return res.status(403).send('username or password does not match');
   }
 });
 
@@ -145,20 +150,25 @@ app.post('/register', (req, res) => {
   const password = req.body.password;
   // If the e-mail or password are empty strings, send 404
   if (!email || !password) {
-    return res.status(404).render('404');
+    return res.status(404).send('Please enter email and password.');
   }
   // register with an email that is already in the users object, send 404
   if (getUserByEmail(users, email)) {
-    return res.status(404).render('404');
+    return res.status(404).send('A user with that email already exists');
   }
+  // Adding salt + hash
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+
   // Move database under if statement above because we want to check email against existing database prior to adding it. 
   users[user_id] = {
     id: user_id,
     email,
-    password,
+    password: hash // update password to hashed password
   };
+  console.log("users[user_id]", users[user_id])
   res.cookie("user_id", user_id);
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 // Cookie-parser
@@ -173,7 +183,7 @@ app.get('/login', (req, res) => {
   const user_id = req.cookies['user_id']
   // if user is logged in, /login will redirect to /urls
   if (req.cookies.user_id) {
-    res.redirect('/urls');
+    return res.redirect('/urls');
   }
   const templateVars = {
     user: users[user_id],
@@ -185,7 +195,7 @@ app.get('/register', (req, res) => {
   const user_id = req.cookies['user_id'];
   // if user is logged in, /register will redirect to /urls
   if (user_id) {
-    res.redirect('/urls');
+    return res.redirect('/urls');
   }
   const templateVars = {
     user: users[user_id],
